bb626ce4659a47fdea00c3a37c26147f41694331
KuroZabulus <trannguyen.quangvinh2004@gmail.com>
2023-03-03 13:15:23 +0700

    PRF192
    PRF192

--- ArrayDemo/Untitled2.cpp
+++ ArrayDemo/Untitled2.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define MAXN 100
 
 void input(int*a, int n);
@@ -7,7 +8,7 @@ void print(int* a, int n);
 void printEven (int* a, int n);
 int main()
 {
-	int a[MAXN];
+	int *a; //prev int a[MAXN];
 	int n;
 	int maxVal;
 	do
@@ -16,6 +17,7 @@ int main()
 		scanf("%d",&n);
 	}
 	while (n<1||n>MAXN);
+	a = (int*)calloc(n,sizeof(int));
 	printf("Enter %d values of array: \n",n);
 	input(a,n);
 	maxVal = max(a,n);
